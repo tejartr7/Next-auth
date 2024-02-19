43628439fe0d967c35abe58ac2990d79bcd7786d
tejartr7 <raghusai126@gmail.com>
2024-02-19 22:19:38 +0530

    rtr commits

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,19 +1,24 @@
+import React, { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
 import { LoginForm } from "@/components/auth/login-form";
 const Page = () => {
-    return (
-        <div className='flex items-center justify-center h-screen font-Poppins'>
-            <div className=' bg-white h-[80vh] w-[80vw] lg:h-[80vh] lg:w-[40vw]' style={{ borderRadius: '10px' }}>
-                <LoginForm
-                    header="Next Auth"
-                    backButtonLabel="Dont have an account?"
-                    backButtonHref="/auth/register" 
-                    showSocial 
-                >
-                    Login form
-                </LoginForm>
-            </div>
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="flex items-center justify-center h-screen font-Poppins">
+        <div
+          className=" bg-white h-[80vh] w-[80vw] lg:h-[80vh] lg:w-[40vw]"
+          style={{ borderRadius: "10px" }}
+        >
+          <LoginForm
+            header="Next Auth"
+            backButtonLabel="Dont have an account?"
+            backButtonHref="/auth/register"
+            showSocial
+          >
+            Login form
+          </LoginForm>
         </div>
-    )
-}
-
-export default Page;
+      </div>
+    </Suspense>
+  );
+};
