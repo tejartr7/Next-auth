ab8a85b0cd6836a46ab35e3ebe19fd60c4d83a69
tejartr7 <raghusai126@gmail.com>
2024-02-19 22:30:15 +0530

    new commits by dev

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,24 +1,31 @@
-import React, { Suspense } from "react";
-import { useSearchParams } from "next/navigation";
+'use client'
+import React,{Suspense} from "react";
 import { LoginForm } from "@/components/auth/login-form";
+import { useSearchParams } from "next/navigation";
+
 const Page = () => {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl");
+
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="flex items-center justify-center h-screen font-Poppins">
-        <div
-          className=" bg-white h-[80vh] w-[80vw] lg:h-[80vh] lg:w-[40vw]"
-          style={{ borderRadius: "10px" }}
+    
+    <div className="flex items-center justify-center h-screen font-Poppins">
+      <div
+        className=" bg-white h-[80vh] w-[80vw] lg:h-[80vh] lg:w-[40vw]"
+        style={{ borderRadius: "10px" }}
+      >
+        <LoginForm
+          header="Next Auth"
+          backButtonLabel="Dont have an account?"
+          backButtonHref="/auth/register"
+          showSocial
         >
-          <LoginForm
-            header="Next Auth"
-            backButtonLabel="Dont have an account?"
-            backButtonHref="/auth/register"
-            showSocial
-          >
-            Login form
-          </LoginForm>
-        </div>
+          Login form
+        </LoginForm>
       </div>
-    </Suspense>
+    </div>
+    
   );
 };
+
+export default Page;
