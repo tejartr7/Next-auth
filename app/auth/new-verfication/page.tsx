c7e7c730dbf0e5fdbbb77a8e27cc32a924cc37c6
tejartr7 <raghusai126@gmail.com>
2024-02-20 22:36:29 +0530

    reset commits

---
+++ app/auth/new-verfication/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { NewVerificationForm } from '@/components/auth/new-verification';
+const Page = () => {
+  return (
+    <div>
+        < NewVerificationForm />
+    </div>
+  )
+}
+
+export default Page
---
+++ app/auth/reset/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ResetForm } from '@/components/auth/reset'
+const Page = () => {
+  return (
+    <div>
+        < ResetForm />
+    </div>
+  )
+}
+
+export default Page
