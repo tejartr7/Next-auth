d976c6537fbb9051ca637e12cbf0e43a35d0a13e
tejartr7 <raghusai126@gmail.com>
2024-02-25 12:07:53 +0530

    commit messages

--- lib/mail.ts
+++ lib/mail.ts
@@ -7,7 +7,7 @@ const domain = process.env.NEXT_PUBLIC_APP_URL;
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
   await resend.emails.send({
     from: "onboarding@resend.dev",
-    to: "sai707nenupavan@gmail.com",
+    to: email,
     subject: "2FA Code",
     html: `<p>Your 2FA code: ${token}</p>`,
   });
@@ -18,7 +18,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   console.log(email);
   await resend.emails.send({
     from: "onboarding@resend.dev",
-    to: "sai707nenupavan@gmail.com",
+    to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
   });
@@ -29,7 +29,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   console.log(email);
   await resend.emails.send({
     from: "onboarding@resend.dev",
-    to: "sai707nenupavan@gmail.com",
+    to: email,
     subject: "Confirm your email",
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
   });
